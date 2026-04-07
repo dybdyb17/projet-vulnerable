9c826b5628095d85df5adfae68beb7bd628123ac
Boudiaf Dybril <dybrilboudiaf14@gmail.com>
2026-04-07 16:14:36 +0200

    add cookies script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,6 @@
 console.log('hello');
 
+document.cookie = "username=Oeschger;";
+document.cookie = "email=email@xss.fr;";
+document.cookie = "password=password;";
 
-/** @info
- * A coller dans le input description pour la demo
- * */
-
-// <script>
-//
-// const p = document.querySelectorAll('p');
-//
-// for (const e of p) {
-//     e.style.color = 'red';
-// }
-// </script>
